7f38bd9d9ad9a542a7d2e2acc7e112470bdf6d87
kim changduk <kimchangduk92@gmail.com>
2017-09-18 22:02:44 +0900

    Q1451, Q2966 추가

---
+++ csharp/Q1451/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1451
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nm = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(item => int.Parse(item)).ToArray();
+            int n = nm[0], m = nm[1];
+            int[,] matrix = new int[n, m];
+            for (int i = 0; i < n; i++)
+            {
+                var nums = Console.ReadLine().ToCharArray().Select(item => item - '0').ToArray();
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i, j] = nums[j];
+                }
+            }
+
+            long result = calculate(matrix, 3, 0, 0, n, m);
+            Console.WriteLine(result);
+        }
+
+        private static long calculate(int[,] matrix,int sliceCount, int sX, int sY, int eX, int eY)
+        {
+            if (sliceCount == 1)
+            {
+                return sumOfRect(matrix, sX, sY, eX, eY);
+            }
+
+            long max = 0;
+            // 4가지 케이스로 1/2로 슬라이스
+            for (var i = sX+1; i < eX; i++)
+            {
+                // [v][ ]
+                max = Math.Max(max, sumOfRect(matrix, sX, sY, i, eY) * calculate(matrix, sliceCount - 1, i, sY, eX, eY));
+                // [ ][v]
+                max = Math.Max(max, sumOfRect(matrix, i, sY, eX, eY) * calculate(matrix, sliceCount - 1, sX, sY, i, eY));
+            }
+            for (var i = sY + 1; i < eY; i++)
+            {
+                // [v]
+                // [ ]
+                max = Math.Max(max, sumOfRect(matrix, sX, sY, eX, i) * calculate(matrix, sliceCount - 1, sX, i, eX, eY));
+                // [ ]
+                // [v]
+                max = Math.Max(max, sumOfRect(matrix, sX, i, eX, eY) * calculate(matrix, sliceCount - 1, sX, sY, eX, i));
+            }
+            return max;
+        }
+
+        private static long sumOfRect(int[,] matrix, int sX, int sY, int eX,int eY)
+        {
+            long sum = 0;
+            for (var i = sX; i < eX; i++) for (var j = sY; j < eY; j++) sum += matrix[i, j];
+            return sum;
+        }
+    }
+}
---
+++ csharp/Q2966/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Q2966
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[] answers = Console.ReadLine().ToCharArray();
+            String[] names = new String[3];
+            names[0] = "Adrian";
+            names[1] = "Bruno";
+            names[2] = "Goran";
+
+            // 상근이는 A, B, C, A, B, C, A, B, C, A, B, C
+            // 창영이는 B, A, B, C, B, A, B, C, B, A, B, C,
+            // 현진이는 C, C, A, A, B, B, C, C, A, A, B, B, ...
+
+            int[] scores = new int[3];
+            for (int i = 0; i < 3; i++) scores[i] = 0;
+
+            int[] submits = new int[3];
+            for (int i = 0; i < n; i++)
+            {
+                char answer = answers[i];
+                int temp = ((i / 2) % 3);
+                submits[0] = 'A' + (char)(i % 3);
+                submits[1] = (i % 2 == 0) ? 'B' : ((i % 4 == 1) ? 'A' : 'C');
+                submits[2] = temp == 0 ? 'C' : (temp == 1 ? 'A' : 'B');
+
+                for (int j = 0; j < 3; j++)
+                {
+                    if (submits[j] == answer)
+                    {
+                        scores[j]++;
+                    }
+                }
+            }
+
+            int max = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                if (max < scores[i]) max = scores[i];
+            }
+            Console.WriteLine(max);
+            for (int i = 0; i < 3; i++)
+            {
+                if (scores[i] == max)
+                    Console.WriteLine(names[i]);
+            }
+        }
+    }
+}
