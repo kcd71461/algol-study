b5e3f0d6021fc10b0919e0b9d1aa938944043bd2
kim changduk <kimchangduk92@gmail.com>
2017-10-16 19:43:30 +0900

    Q1543 추가

---
+++ csharp/Q1543/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Q1543
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string search = Console.ReadLine();
+            int count = 0;
+            int index;
+            while ((index = input.IndexOf(search)) >= 0)
+            {
+                count++;
+                input = input.Substring(index + search.Length);
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
